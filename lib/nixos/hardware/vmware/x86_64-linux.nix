16e8b3aec96e40df3e610427a3ddf4f34dcd3231
Justin Solo <github@justinsolo.dev>
2025-01-04 13:22:54 -0800

    fix hardware

--- lib/nixos/hardware/vmware/x86_64-linux.nix
+++ lib/nixos/hardware/vmware/x86_64-linux.nix
@@ -12,7 +12,7 @@
 
   boot.initrd.availableKernelModules = ["ata_piix" "mptspi" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm" "kvm_intel"];
+  boot.kernelModules = [];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
